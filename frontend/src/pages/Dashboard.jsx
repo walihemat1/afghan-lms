c9c269b4fb07dbcb6409c8c86b7b3ccecfd74ad7
walihemat1 <ahmadwalihemat67@gmail.com>
2025-08-06 22:54:46 +0430

    assigned tasks

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
+import "./dashboard.css";
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return <div className="bg-blue-400 text-gray-900">Dashboard</div>;
 }
 
 export default Dashboard;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,20 @@
 function Home() {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+        <div>
+          <h1>LMS</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. At
+            inventore, delectus, accusantium perspiciatis ratione dignissimos
+            blanditiis voluptatem nam eius, consequatur unde numquam aspernatur
+            veritatis provident pariatur porro magnam molestiae ducimus.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 // by exporting we can import this in other files
